52f7b25c02f5cec68c7eceb99e5913e371fd66da
LuciferTheFirstt <dimonator67@gmail.com>
2019-10-14 12:57:47 +0300

    all 2.0 with second test

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -5,3 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(CH "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab01/test_json")
+add_test(CN "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab01/main")
